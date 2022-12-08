4a8fff15f4c0d98697b1fc7381d58d2cbab62e5a
dang_woo <kdw3917ss@naver.com>
2022-12-08 17:35:57 +0900

    .

---
+++ class/03-javascript/연습-04-variable-if.js
@@ -0,0 +1,15 @@
+console.log(1 + 1)
+console.log(1 +"만원")
+console.log(1 + "1")
+console.log(1 - "1")
+console.log("김" + "동우")
+console.log("123" == 123 )
+console.log(123 === "123")
+//and
+console.log(true && true)
+console.log(true && false)
+// or
+console.log(false || true)
+// not
+console.log(!false)
+console.log(!true)
